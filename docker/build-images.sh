4d32553c9cfdafc1c83334dca7bb5952ecacb7e4
phil-peshin <phil.peshin@gmail.com>
2018-06-12 22:13:08 -0700

    AWS code build

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -f dockerfiles/base-os .. --tag starsona-base-os
-docker build -f dockerfiles/starsona .. --tag starsona
+docker build -f dockerfiles/base-httpd .. --tag starsona-base-httpd
+docker build -f dockerfiles/base-backend .. --tag starsona-base-backend
