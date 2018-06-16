9be2730fae5ba36a31c94b73a87f3f8e628b4591
phil-peshin <phil.peshin@gmail.com>
2018-06-15 22:25:04 -0700

    AWS cloud formation

---
+++ utilities/constants.py
@@ -0,0 +1,11 @@
+PASSWORD_MIN_LENGTH = 8
+PASSWORD_MAX_LENGTH = 32
+IOS_UNIVERSAL_FILE_PATH = "../.well-known/apple-app-site-association"
+ANDROID_CONFIG_KEY = "android_version"
+IOS_CONFIG_KEY = "ios_version"
+ANDROID = "android"
+IOS = "ios"
+BASE_URL = ""
+SHORT_BASE_URL = ""
+REDIRECT_LINK = ""
+ANDROID_UNIVERSAL_FILE_PATH = "../.well-known/assetlinks.json"
