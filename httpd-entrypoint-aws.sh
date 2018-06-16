7e0ff71e3c43109a5e7ec6b8eb70856adb0e2470
phil-peshin <phil.peshin@gmail.com>
2018-06-16 00:14:10 -0700

    AWS cloud formation

--- httpd-entrypoint-aws.sh
+++ httpd-entrypoint-aws.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i -r "s/(^[ \t]+ProxyPass.*\/[ \t]+http:\/\/)[a-z].*(:[0-9]+\/.*$)/\1${PRIVATE_LB_HOSTNAME}\2/g" /etc/apache2/sites-enabled/000-default.conf
+#sed -i -r "s/(^[ \t]+ProxyPass.*\/[ \t]+http:\/\/)[a-z].*(:[0-9]+\/.*$)/\1${PRIVATE_LB_HOSTNAME}\2/g" /etc/apache2/sites-enabled/000-default.conf
+sed -i -r "s/(^[ \t]+RewriteRule.*[ \t]+http:\/\/)[a-z].*(:[0-9]+\/.*$)/\1${PRIVATE_LB_HOSTNAME}\2/g" /etc/apache2/sites-enabled/000-default.conf
 
 echo "Stopping existing apache if needed"
 /usr/sbin/apachectl stop || true
