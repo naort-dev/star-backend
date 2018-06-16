59af9f8c4e8c3b3699b90e36cd1ec774e42d4f42
phil-peshin <phil.peshin@gmail.com>
2018-06-16 01:07:12 -0700

    switched to nginx

--- build-docker-base.sh
+++ build-docker-base.sh
@@ -2,6 +2,7 @@
 set -e
 
 docker build -f docker/dockerfiles/base-os --tag ${REGISTRY}starsona-base-os${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
-docker build -f docker/dockerfiles/base-httpd --tag ${REGISTRY}starsona-base-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
+#docker build -f docker/dockerfiles/base-httpd --tag ${REGISTRY}starsona-base-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
+docker build -f docker/dockerfiles/base-nginx --tag ${REGISTRY}starsona-base-nginx${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 docker build -f docker/dockerfiles/base-backend --tag ${REGISTRY}starsona-base-backend${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 
