8a3f7a3adb06640bcca330a61eb8dc4dba8d5bc8
phil-peshin <phil.peshin@gmail.com>
2018-06-15 22:17:44 -0700

    AWS cloud formation

--- build-docker-base.sh
+++ build-docker-base.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 docker build -f docker/dockerfiles/base-os --tag ${REGISTRY}starsona-base-os${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 docker build -f docker/dockerfiles/base-httpd --tag ${REGISTRY}starsona-base-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 docker build -f docker/dockerfiles/starsona-celery --tag ${REGISTRY}starsona-celery${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 docker build -f docker/dockerfiles/starsona-gunicorn --tag ${REGISTRY}starsona-gunicorn${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 id=$(docker create ${REGISTRY}starsona-gunicorn${IMAGE_TAG})
 docker cp $id:/starsona/static .
-docker build -f docker/dockerfiles/starsona-httpd --tag ${REGISTRY}starsona-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
+docker build -f docker/dockerfiles/starsona-httpd --tag ${REGISTRY}starsona-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} --build-arg DEPLOYMENT_TYPE=${DEPLOYMENT_TYPE:-swarm} .
 docker build -f docker/dockerfiles/starsona-migration --tag ${REGISTRY}starsona-migration${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 
