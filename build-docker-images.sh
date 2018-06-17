c0ca730c7646d8581c483cf7ba04e458181e33bf
phil-peshin <phil.peshin@gmail.com>
2018-06-16 17:53:29 -0700

    https implementation

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -5,7 +5,6 @@ docker build -f docker/dockerfiles/starsona-celery --tag ${REGISTRY}starsona-cel
 docker build -f docker/dockerfiles/starsona-gunicorn --tag ${REGISTRY}starsona-gunicorn${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 id=$(docker create ${REGISTRY}starsona-gunicorn${IMAGE_TAG})
 docker cp $id:/starsona/static .
-#docker build -f docker/dockerfiles/starsona-httpd --tag ${REGISTRY}starsona-httpd${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} --build-arg DEPLOYMENT_TYPE=${DEPLOYMENT_TYPE:-swarm} .
-docker build -f docker/dockerfiles/starsona-nginx --tag ${REGISTRY}starsona-nginx${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} --build-arg DEPLOYMENT_TYPE=${DEPLOYMENT_TYPE:-aws} .
+docker build -f docker/dockerfiles/starsona-nginx-${DEPLOYMENT_TYPE:-swarm} --tag ${REGISTRY}starsona-nginx-${DEPLOYMENT_TYPE:-swarm}${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 docker build -f docker/dockerfiles/starsona-migration --tag ${REGISTRY}starsona-migration${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
 
