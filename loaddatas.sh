8923fc272f0f5740262a4eab9f716e5844606538
phil-peshin <phil.peshin@gmail.com>
2018-06-22 08:39:25 -0700

    call manage.py with python, not directly in case of exec permission is missing

--- loaddatas.sh
+++ loaddatas.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-./manage.py shell -c "import os; from django.contrib.auth import get_user_model;\
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model;\
      User = get_user_model(); \
      User.objects.filter(username=os.environ.get('ADMIN_USERNAME'), is_superuser=True).delete(); \
      User.objects.create_superuser(os.environ.get('ADMIN_USERNAME'), os.environ.get('ADMIN_PASSWORD'))"
-./manage.py loaddata config/fixtures/config.json
-./manage.py loaddata role/fixtures/features.json
-./manage.py loaddata role/fixtures/roles.json
-./manage.py loaddata role/fixtures/role_feature_mapping.json
-./manage.py loaddata stargramz/fixtures/relations.json
-./manage.py loaddata stargramz/fixtures/occasions.json
-./manage.py loaddata stargramz/fixtures/orderrelations.json
-./manage.py loaddata users/fixtures/profession.json
+python manage.py loaddata config/fixtures/config.json
+python manage.py loaddata role/fixtures/features.json
+python manage.py loaddata role/fixtures/roles.json
+python manage.py loaddata role/fixtures/role_feature_mapping.json
+python manage.py loaddata stargramz/fixtures/relations.json
+python manage.py loaddata stargramz/fixtures/occasions.json
+python manage.py loaddata stargramz/fixtures/orderrelations.json
+python manage.py loaddata users/fixtures/profession.json
