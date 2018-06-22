39b4841b957f1e4e1a100f701647a4dbff30a009
phil-peshin <phil.peshin@gmail.com>
2018-06-21 22:41:58 -0700

    migrations and loading data

--- loaddatas.sh
+++ loaddatas.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-./manage.py createsuperuser
+./manage.py shell -c "import os; from django.contrib.auth import get_user_model;\
+     User = get_user_model(); \
+     User.objects.filter(username=os.environ.get('ADMIN_USERNAME'), is_superuser=True).delete(); \
+     User.objects.create_superuser(os.environ.get('ADMIN_USERNAME'), os.environ.get('ADMIN_PASSWORD'))"
 ./manage.py loaddata config/fixtures/config.json
 ./manage.py loaddata role/fixtures/features.json
 ./manage.py loaddata role/fixtures/roles.json
