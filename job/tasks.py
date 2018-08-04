f6e5e7093541e9b9f1ebfe3465385b2681ee3047
Qburst Starsona <39151032+qburst-starsona@users.noreply.github.com>
2018-08-04 22:15:26 +0530

    Added exception

--- job/tasks.py
+++ job/tasks.py
@@ -1082,7 +1082,10 @@ def process_audio_file(audio, audio_root):
         new_audio_file = "%s%s.mp3" % (audio_root, name)
         convert_audio_file(audio_file, new_audio_file)
         SendMail('Audio Conversion', 'Audio file is %s' % new_audio_file, sender_email=sender_email, to='akhilns@qburst.com')
-        upload_image_s3(new_audio_file, s3_file_name)
+        try:
+            upload_image_s3(new_audio_file, s3_file_name)
+        except Exception as e:
+            SendMail('Audio Conversion', 'Audio file is %s' % str(e), sender_email=sender_email, to='akhilns@qburst.com')
     return s3_file_name
 
 
