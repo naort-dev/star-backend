f502e10b8c393c3d951fab8f035b9d33dab21faf
phil-peshin <phil.peshin@gmail.com>
2018-08-07 21:06:11 -0700

    shared VPC

--- aws/scripts/run-ecs-task.sh
+++ aws/scripts/run-ecs-task.sh
@@ -2,7 +2,7 @@
 set -e
 
 stack_output=$(mktemp)
-aws cloudformation describe-stacks --stack-name $1-backend-vpc > $stack_output
+aws cloudformation describe-stacks --stack-name $1 > $stack_output
 
 CLUSTER=`jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "ClusterName").OutputValue' $stack_output`
 SUBNET1=`jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PrivateSubnetOne").OutputValue' $stack_output`
@@ -12,7 +12,7 @@ rm $stack_output
 
 run_task_output=$(mktemp)
 aws ecs run-task --cluster $CLUSTER \
-    --task-definition $1-backend-app-$2 \
+    --task-definition $2-backend-app-$3 \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[$SUBNET1,$SUBNET2],securityGroups=[$SECURITY_GROUP],assignPublicIp=DISABLED}" \
     > $run_task_output
@@ -23,6 +23,6 @@ aws ecs wait tasks-stopped --cluster $CLUSTER --tasks $TASK_ID
 EXIT_CODE=`aws ecs describe-tasks --cluster $CLUSTER --tasks $TASK_ID | jq -r '.tasks[0].containers[0].exitCode'`
 rm $run_task_output
 
-aws logs get-log-events --log-group-name $1-backend-app --log-stream-name backend/$2/$TASK_ID | jq -r .events[].message
+aws logs get-log-events --log-group-name $2-backend-app --log-stream-name backend/$3/$TASK_ID | jq -r .events[].message
 
 exit $EXIT_CODE
