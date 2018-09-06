dc5928158e673a67aa8a76a488d5d8d0e7bd3ffe
Akhilraj NS <akhilrajns@gmail.com>
2018-09-06 20:08:25 +0530

    Updated refferal name and vanity name without special chars

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,6 @@
 import random
 import string
+import re
 
 
 def generate_referral_unique_code(instance):
@@ -7,8 +8,10 @@ def generate_referral_unique_code(instance):
         Generate the referral code for users
     """
     chars = string.digits
-    name = ''.join(e for e in instance.get_short_name() if e.isalnum())[:5]
-    code = name.upper()
+    name = re.sub('[^A-Za-z0-9]+', '', instance.get_short_name())
+    if len(name) == 0:
+        name = re.sub('[^A-Za-z0-9]+', '', get_vanity_from_email(instance))
+    code = name.upper()[:5]
     size = 8 - len(code)
     referral_code = ''.join(random.choice(chars) for _ in range(size))
     code = str(code) + (referral_code[:8])
@@ -36,8 +39,11 @@ def generate_vanity_url(instance, value_exist=0):
     :return: vanity url name
     """
     from users.models import VanityUrl
-    name = ''.join(e for e in get_full_name(instance) if e.isalpha())
+
+    name = re.sub('[^A-Za-z0-9]+', '', get_full_name(instance))
     code = name.lower()
+    if len(code) == 0:
+        code = re.sub('[^A-Za-z0-9]+', '', get_vanity_from_email(instance))
     if value_exist > 0 or len(code) == 0:
         chars = string.digits
         random_code = ''.join(random.choice(chars) for _ in range(value_exist))
@@ -56,4 +62,14 @@ def get_full_name(instance):
     """
     if instance.show_nick_name and instance.nick_name:
         return instance.nick_name
-    return ' '.join(filter(None, [instance.first_name, instance.last_name]))
+    return ''.join(filter(None, [instance.first_name, instance.last_name]))
+
+
+def get_vanity_from_email(instance):
+    """
+    Get the vanity name from email
+    :param instance:
+    :return: email name
+    """
+    email = instance.email
+    return email.split("@")[0]
