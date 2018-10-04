9e8b62b7a9eac2c93c83b2290d19f2a5ca78cfaa
phil-peshin <phil.peshin@gmail.com>
2018-10-03 22:35:54 -0700

    updated relationships order

---
+++ stargramz/fixtures/generate_orderrelations.py
@@ -0,0 +1,63 @@
+import json
+from collections import OrderedDict
+from pprint import pprint
+
+with open('occasions.json') as f:
+    occasions = json.load(f)
+
+with open('relations.json') as f:
+    relations = json.load(f)
+
+relations_dict = {}
+
+
+for r in relations:
+    relations_dict[r['fields']['title']] = r
+
+
+ordered_relations = [
+    'Friend',
+    'Mother',
+    'Father',
+    'Daughter',
+    'Son',
+    'Sister',
+    'Brother',
+    'Wife',
+    'Husband',
+    'Grandmother',
+    'Grandfather',
+    'Girlfriend',
+    'Boyfriend',
+    'Aunt',
+    'Uncle',
+    'Niece',
+    'Nephew',
+    'Cousin',
+    'Coworker',
+]
+
+output = []
+pk = 1
+
+for occ in occasions:
+    order = 1
+    for rel in ordered_relations:
+        item = OrderedDict([
+            ("model", "stargramz.orderrelationship"),
+            ("pk", pk),
+            ("fields",
+                OrderedDict([
+                    ("order", order),
+                    ("occasion_id", occ['pk']),
+                    ("relation_id", relations_dict[rel]['pk'])
+                ])
+            )
+        ])
+        output.append(item)
+        order += 1
+        pk += 1
+
+with open('orderrelations.json', 'w') as f:
+    f.write(json.dumps(output, indent=2))
+
