9d56103600d5667cda1829fb2e80ca41c93d121d
Akhilraj NS <akhilrajns@gmail.com>
2018-11-14 17:40:33 +0530

    Status

--- payments/serializer.py
+++ payments/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import StarsonaTransaction, TipPayment
+from .models import StarsonaTransaction, TipPayment, PaymentPayout, PAYOUT_STATUS
 from stargramz.serializer import TransactionStargramzSerializer
 from hashids import Hashids
 hashids = Hashids(min_length=8)
@@ -28,10 +28,17 @@ class StarsonaTransactionSerializer(serializers.ModelSerializer):
     starsona = TransactionStargramzSerializer(read_only=True)
     amount = serializers.DecimalField(read_only=True, max_digits=7, decimal_places=2)
     id = serializers.CharField(read_only=True, source='order_id')
+    payout_status = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = StarsonaTransaction
-        fields = ('id', 'starsona', 'created_date', 'amount')
+        fields = ('id', 'starsona', 'created_date', 'amount', 'payout_status')
+
+    def get_payout_status(self, obj):
+        try:
+            return PAYOUT_STATUS.get_label(PaymentPayout.objects.values_list('status', flat=True).get(transaction_id=obj.id))
+        except Exception:
+            return PAYOUT_STATUS.get_label(1)
 
 
 class BookingValidate(serializers.ModelSerializer):
