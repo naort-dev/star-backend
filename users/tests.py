9fa919810acf7b1673562c2544c4027ca60947fd
Akhilraj NS <akhilrajns@gmail.com>
2019-01-25 11:22:32 +0530

    Adding unit test for Users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class UsersTest(TestCase):
+    """
+        Unit test file for testing Users app
+    """
+
+    def setUp(self):
+        self.client = Client(HTTP_DEVICE='web', HTTP_VERSION='1.0')
+
+    def test_professions(self):
+        response = self.client.get(reverse('profession'), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
