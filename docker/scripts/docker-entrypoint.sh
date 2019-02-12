da6ab0de1eb176750285e30dd7ec79eb861a103d
Starsona Build <build@starsona.com>
2019-02-11 22:17:56 -0800

    fixed local docker deployment

--- docker/scripts/docker-entrypoint.sh
+++ docker/scripts/docker-entrypoint.sh
@@ -6,6 +6,35 @@ if [[ -z "$ENV" ]]; then
     exit 1
 fi
 
+#
+# This is only for local Docker setup, when run in AWS ECS instance role set automatically
+#
+if [ -n "$AWS_INSTANCE_ROLE" ] \
+    && [ -n "$AWS_ACCESS_KEY_ID" ] \
+    && [ -n "$AWS_SECRET_ACCESS_KEY" ] \
+    && [ -n "$AWS_DEFAULT_REGION" ]; then
+echo "Creating local AWS credentials files..."
+mkdir ~/.aws
+
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+EOF
+
+cat <<EOF > ~/.aws/config
+[default]
+region=$AWS_DEFAULT_REGION
+role_arn=$AWS_INSTANCE_ROLE
+source_profile=default
+EOF
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_DEFAULT_REGION
+
+fi
+
 cp .well-known/apple-app-site-association.$ENV .well-known/apple-app-site-association
 cp templates/home/apps.html.$ENV templates/home/apps.html
 cp config/fixtures/config.json.$ENV config/fixtures/config.json
