ebfcd39f0e4fbd0512987d879a553f543e70aae5
naort-dev <naor.tzairi.upwork@gmail.com>
2019-05-14 12:21:20 +0530

    The elastic end point changed

--- config/serializer.py
+++ config/serializer.py
@@ -20,6 +20,12 @@ class ConfigSerializer(serializers.ModelSerializer):
             if obj.key == 'decline_reasons':
                 return json.loads(obj.value)
             elif obj.key in environmental_variables:
+                if obj.key is 'elastic_search_endpoint':
+                    value = os.environ.get(environmental_variables[obj.key], None)
+                    if value:
+                        return value.replace(":443", "/")
+                    else:
+                        return value
                 return os.environ.get(environmental_variables[obj.key], None)
         except Exception as e:
             return obj.value
